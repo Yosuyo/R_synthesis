dc45db355056e0613e93f80112d19f534f368ead
Yosuyo <porepore1996@gmail.com>
2019-05-22 13:55:49 +0900

    描画機能の土台が完成

--- draw.js
+++ draw.js
@@ -3,11 +3,10 @@
 var canvas = document.getElementById("canvas2");
 canvas.oncontextmenu = function () {
     //テキストボックスを開き元素記号を入力する処理
+    console.log("右");
     return false;
 };
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "rgba(255,0,255,0.1)";
-ctx.fillRect(0, 0, 800, 760);
 
 var w = 50;
 var m = 10;
@@ -16,63 +15,167 @@ var mouseX, mouseY;
 var downX, downY;
 var drag = false;
 
-function line(x, y) {
-    ctx.strokeStyle = "rgb(0,0,0)";
-    ctx.lineWidth = 3;
-    ctx.beginPath();
-    ctx.moveTo(downX, downY);
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    drag = false;
+var list = [];
+
+var mode = 0;
+
+function def() {
+    mode = 0;
+}
+function inp() {
+    mode = 1;
 }
 
 canvas.onmousedown = function (e) {
     mouseX = e.offsetX;
     mouseY = e.offsetY;
+    function begin(x, y) {
+        if (mode == 0) {
+            downX = x;
+            downY = y;
+            drag = true;
+        } else if (mode == 1) {
+            ctx.fillStyle = "rgb(240,240,240)";
+            ctx.fillRect(x - 10, y - 10, 20, 20);
+            ctx.fillStyle = "rgb(0,0,0)";
+            ctx.font = "20px serif";
+            ctx.fillText("X", x - 7, y + 8);
+        }
+    }
     for (var i = 0; i < 9; i++) {
         for (var j = 0; j < 16; j++) {
             if (j * w + m - 5 <= mouseX && mouseX <= j * w + m + 5 && i * Math.sqrt(3) * w + m - 5 <= mouseY && mouseY <= i * Math.sqrt(3) * w + m + 5) {
-                ctx.fillStyle = "rgba(0,0,0,1)";
-                ctx.beginPath();
-                ctx.arc(j * w + m, i * Math.sqrt(3) * w + m, 3, 0, 2 * Math.PI, true);
-                ctx.fill();
-                downX = j * w + m;
-                downY = i * Math.sqrt(3) * w + m;
-                drag = true;
-                break;
+                begin(j * w + m, i * Math.sqrt(3) * w + m);
+                return;
             }
             if (j * w + (w / 2) + m - 5 <= mouseX && mouseX <= j * w + (w / 2) + m + 5 && (i + 0.5) * Math.sqrt(3) * w + m - 5 <= mouseY && mouseY <= (i + 0.5) * Math.sqrt(3) * w + m + 5) {
-                ctx.fillStyle = "rgba(0,0,0,1)";
-                ctx.beginPath();
-                ctx.arc(j * w + (w / 2) + m, (i + 0.5) * Math.sqrt(3) * w + m, 3, 0, 2 * Math.PI, true);
-                ctx.fill();
-                downX = (j + 0.5) * w + m;
-                downY = (i + 0.5) * Math.sqrt(3) * w + m;
-                drag = true;
-                break;
+                begin((j + 0.5) * w + m, (i + 0.5) * Math.sqrt(3) * w + m);
+                return;
             };
         }
     }
 };
 canvas.onmouseup = function (e) {
-    console.log(1);
+    var flug = 0;
     mouseX = e.offsetX;
     mouseY = e.offsetY;
-    if (mouseX < downX) {
-        if ((mouseY - downY) / (mouseX - downX) < (-1 / Math.sqrt(3))) {
-            line(downX - (0.5 * w), downY + (0.5 * Math.sqrt(3) * w));
-        } else if ((mouseY - downY) / (mouseX - downX) > 1 / Math.sqrt(3)) {
-            line(downX - (0.5 * w), downY - (0.5 * Math.sqrt(3) * w));
+    function line(x, y) {
+        ctx.strokeStyle = "rgb(0,0,0)";
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.moveTo(downX, downY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        drag = false;
+        list.push([Math.round(downX), Math.round(downY), Math.round(x), Math.round(y), 1]);
+        console.log(list);
+    }
+    function double(x, y) {
+        ctx.strokeStyle = "rgb(0,0,0)";
+        ctx.lineWidth = 9;
+        ctx.beginPath();
+        ctx.moveTo(downX, downY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.strokeStyle = "rgb(240,240,240)";
+        ctx.lineWidth = 3;
+        ctx.moveTo(downX, downY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        drag = false;
+        list.push([Math.round(downX), Math.round(downY), Math.round(x), Math.round(y), 2]);
+        flug = 0;
+    }
+    function triple(x, y) {
+        ctx.strokeStyle = "rgb(0,0,0)";
+        ctx.lineWidth = 15;
+        ctx.beginPath();
+        ctx.moveTo(downX, downY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.strokeStyle = "rgb(240,240,240)";
+        ctx.lineWidth = 9;
+        ctx.moveTo(downX, downY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.strokeStyle = "rgb(0,0,0)";
+        ctx.lineWidth = 3;
+        ctx.moveTo(downX, downY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        drag = false;
+        list.push([Math.round(downX), Math.round(downY), Math.round(x), Math.round(y), 3]);
+        flug = 0;
+    }
+    function check(x, y) {
+        for (var i = 0; i < list.length; i++) {
+            if (list[i][0] == Math.round(downX) && list[i][1] == Math.round(downY) && list[i][2] == Math.round(x) && list[i][3] == Math.round(y) && list[i][4] == 1) {
+                flug = 1;
+                break;
+            } else if (list[i][0] == Math.round(x) && list[i][1] == Math.round(y) && list[i][2] == Math.round(downX) && list[i][3] == Math.round(downY) && list[i][4] == 1) {
+                flug = 1;
+                break;
+            }
+        };
+        for (var i = 0; i < list.length; i++) {
+            if (list[i][0] == Math.round(downX) && list[i][1] == Math.round(downY) && list[i][2] == Math.round(x) && list[i][3] == Math.round(y) && list[i][4] == 2) {
+                flug = 2;
+                break;
+            } else if (list[i][0] == Math.round(x) && list[i][1] == Math.round(y) && list[i][2] == Math.round(downX) && list[i][3] == Math.round(downY) && list[i][4] == 2) {
+                flug = 2;
+                break;
+            }
+        };
+    }
+    function draw() {
+        check(x, y);
+        if (flug == 1) {
+            double(x, y);
+        } else if (flug == 2) {
+            triple(x, y);
         } else {
-            line(downX - w, downY);
+            line(x, y);
         }
-    } else {
-        if ((mouseY - downY) / (mouseX - downX) < (-1 / Math.sqrt(3))) {
-            line(downX + (0.5 * w), downY - (0.5 * Math.sqrt(3) * w));
-        } else if ((mouseY - downY) / (mouseX - downX) > 1 / Math.sqrt(3)) {
-            line(downX + (0.5 * w), downY + (0.5 * Math.sqrt(3) * w));
+    }
+    if (drag == true) {
+        if (mouseX < downX) {
+            if ((mouseY - downY) / (mouseX - downX) < (-1 / Math.sqrt(3))) {
+                var x = downX - (0.5 * w);
+                var y = downY + (0.5 * Math.sqrt(3) * w);
+                draw()
+                return;
+            } else if ((mouseY - downY) / (mouseX - downX) > 1 / Math.sqrt(3)) {
+                var x = downX - (0.5 * w);
+                var y = downY - (0.5 * Math.sqrt(3) * w);
+                var flug = 0;
+                draw();
+                return;
+            } else {
+                var x = downX - w;
+                var y = downY;
+                draw();
+                return;
+            }
         } else {
-            line(downX + w, downY);
+            if ((mouseY - downY) / (mouseX - downX) < (-1 / Math.sqrt(3))) {
+                var x = downX + (0.5 * w);
+                var y = downY - (0.5 * Math.sqrt(3) * w);
+                draw();
+                return;
+            } else if ((mouseY - downY) / (mouseX - downX) > 1 / Math.sqrt(3)) {
+                var x = downX + (0.5 * w);
+                var y = downY + (0.5 * Math.sqrt(3) * w);
+                draw();
+                return;
+            } else {
+                var x = downX + w;
+                var y = downY;
+                draw();
+                return;
+            }
         }
     }
 };
--- grid.js
+++ grid.js
@@ -21,10 +21,10 @@ function draw2() {
     for (var i = 0; i < 9; i++) {
         for (var j = 0; j < 16; j++) {
             ctx.beginPath();
-            ctx.arc(j * w + m, i * Math.sqrt(3) * w + m, 3, 0, 2 * Math.PI, true);
+            ctx.arc(j * w + m, i * Math.sqrt(3) * w + m, 1, 0, 2 * Math.PI, true);
             ctx.stroke();
             ctx.beginPath();
-            ctx.arc(j * w + (w / 2) + m, (i + 0.5) * Math.sqrt(3) * w + m, 3, 0, 2 * Math.PI, true);
+            ctx.arc(j * w + (w / 2) + m, (i + 0.5) * Math.sqrt(3) * w + m, 1, 0, 2 * Math.PI, true);
             ctx.stroke();
         }
     }
